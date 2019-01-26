7225b742bb2659f3eeeae883de00b1bff6e60909
Ragnar Brynjúlfsson <me@ragnarb.com>
2019-01-26 17:38:04 +0100

    Made hiding and showing menu it's own functions, so the menu can be hidden when you navigate panels/pages. Added parsing of panel and page information from SVG files.

--- js/reveal.js
+++ js/reveal.js
@@ -1,5 +1,8 @@
 var speed = 500
 
+var page = {};
+var panels = [];
+
 var prev = document.getElementById('prevbtn');
 var menu = document.getElementById('menubtn');
 var next = document.getElementById('nextbtn');
@@ -7,10 +10,92 @@ var next = document.getElementById('nextbtn');
 var nexttime = false
 var menuvis = true
 
+// MENU
+
+function menuHide() {
+    var hidemenu = anime({
+	targets: '#menu',
+	translateY: -60,
+	duration: speed,
+	easing: 'easeOutExpo',
+	loop: false
+    });
+}
+
+function menuShow() {
+    var showmenu = anime({
+	targets: '#menu',
+	translateY: 0,
+	duaration: speed,
+	easing: 'easeOutExpo',
+	loop: false
+    });
+}
+
+function menuToggle() {
+    if (menuvis == true) {
+	menuHide();
+    } else {
+	menuShow();
+    }
+    menuvis = !menuvis;
+}
+
+
+// NAVIGATION
+
+// Parse SVG
+
+
+function sortPanels(a,b) {
+    if (a.id < b.id)
+	return -1;
+    if (a.id > b.id)
+	return 1;
+    return 0
+}
+
+function readSVG() {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+	if (this.readyState == 4 && this.status == 200) {
+	    parseSVG(this);
+	}
+    };
+    xmlhttp.open("GET", "./images/the-pillow-method-1.svg", true);
+    xmlhttp.send();
+}
+
+
+function parseSVG(xml) {
+    page = {};
+    panels = [];
+    var x, i, xmlDoc, txt;
+    xmlDoc = xml.responseXML;
+    svg = xmlDoc.getElementsByTagName('svg');
+    page.width = parseFloat(svg[0].getAttribute('width').replace(/[^0-9.]/g,''));
+    page.height = parseFloat(svg[0].getAttribute('height').replace(/[^0-9.]/g,''));
+    console.log(page);
+    rect = xmlDoc.getElementsByTagName("rect");
+    for (i = 0; i<rect.length; i++) {
+	var panel = {}
+	panel.id = rect[i].getAttribute('id');
+	panel.x = parseFloat(rect[i].getAttribute('x'));
+	panel.y = parseFloat(rect[i].getAttribute('y'));
+	panel.height = parseFloat(rect[i].getAttribute('height'));
+	panel.width = parseFloat(rect[i].getAttribute('width'));
+	panels.push(panel)
+    }
+    panels.sort(sortPanels)
+    console.log(panels);
+}
+
 next.onclick = function () {
+    console.log(page);
+    menuHide();
     if (nexttime == false) {
 	var zoomin = anime({
-	    targets: '#pagecontainer',
+	    targets: '.page',
 	    scale: 2.0,
 	    translateX: 200.0,
 	    duration: speed,
@@ -19,7 +104,7 @@ next.onclick = function () {
 	});
     } else {
 	var zoomout = anime({
-	    targets: "#pagecontainer",
+	    targets: ".page",
 	    scale: 1.0,
 	    translateX: 0.0,
 	    duration: speed,
@@ -31,33 +116,9 @@ next.onclick = function () {
 }
 
 menu.onclick = function () {
-    if (menuvis == true) {
-	var hidemenu = anime({
-	    targets: '#menu',
-	    translateY: -60,
-	    duration: speed,
-	    easing: 'easeOutExpo',
-	    loop: false
-	});
-    } else {
-	var showmenu = anime({
-	    targets: '#menu',
-	    translateY: 0,
-	    duaration: speed,
-	    easing: 'easeOutExpo',
-	    loop: false
-	});
-    }
-    menuvis = !menuvis;
+    menuToggle();
 }
 
 prev.onclick = function () {
-    var zoomout = anime({
-	targets: "#pagecontainer",
-	scale: 0.5,
-	translateX: 2.0,
-	duration: speed,
-	easing: 'easeOutExpo',
-	loop: false,
-    });
+    readSVG();
 }
