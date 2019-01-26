18917388aaed7b277b8671a4ffc2ee769aa276e0
Ragnar Brynjúlfsson <me@ragnarb.com>
2019-01-26 13:39:42 +0100

    Page image now scales correctly to full width and height, while maintaining aspect ratio. Added a top menu area, activated by clicking the middle of the page.

--- js/reveal.js
+++ js/reveal.js
@@ -1,7 +1,11 @@
+var speed = 500
+
 var prev = document.getElementById('prevbtn');
+var menu = document.getElementById('menubtn');
 var next = document.getElementById('nextbtn');
 
 var nexttime = false
+var menuvis = true
 
 next.onclick = function () {
     if (nexttime == false) {
@@ -9,21 +13,42 @@ next.onclick = function () {
 	    targets: '#pagecontainer',
 	    scale: 2.0,
 	    translateX: 200.0,
-	    duration: 750,
+	    duration: speed,
 	    easing: 'easeOutExpo',
 	    loop: false
 	});
     } else {
-	    var zoomout = anime({
-	targets: "#pagecontainer",
-	scale: 1.0,
-	translateX: 0.0,
-	duration: 750,
-	easing: 'easeOutExpo',
-	loop: false,
-	    });
+	var zoomout = anime({
+	    targets: "#pagecontainer",
+	    scale: 1.0,
+	    translateX: 0.0,
+	    duration: speed,
+	    easing: 'easeOutExpo',
+	    loop: false,
+	});
     }
     nexttime = !nexttime;
+}
+
+menu.onclick = function () {
+    if (menuvis == true) {
+	var hidemenu = anime({
+	    targets: '#menu',
+	    translateY: -60,
+	    duration: speed,
+	    easing: 'easeOutExpo',
+	    loop: false
+	});
+    } else {
+	var showmenu = anime({
+	    targets: '#menu',
+	    translateY: 0,
+	    duaration: speed,
+	    easing: 'easeOutExpo',
+	    loop: false
+	});
+    }
+    menuvis = !menuvis;
 }
 
 prev.onclick = function () {
@@ -31,7 +56,7 @@ prev.onclick = function () {
 	targets: "#pagecontainer",
 	scale: 0.5,
 	translateX: 2.0,
-	duration: 750,
+	duration: speed,
 	easing: 'easeOutExpo',
 	loop: false,
     });
