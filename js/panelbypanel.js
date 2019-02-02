b38618d0d85aa91d168e3800f2a4f899e3e08e0a
Ragnar Brynjúlfsson <ragtag@ragtag.net>
2019-02-02 20:45:53 +0100

    Panel by panel navigation working. Cleaned out old code. Page to page not implemented yet.

--- js/panelbypanel.js
+++ js/panelbypanel.js
@@ -1,6 +1,9 @@
 'use strict';
-// Set the speed of animatons.
-//const speed = 2000;
+
+// Set the speed of animations.
+const speed = 750;
+
+
 
 class PanelByPanel {
     constructor(comic) {
@@ -16,6 +19,7 @@ class PanelByPanel {
 	
 	this.artist = new Draw(this.comic);
 	this.artist.focus();
+	window.onresize = function() { self.artist.focus() }
     }
 
     next() {
@@ -59,32 +63,48 @@ class Draw {
 	this.imageWidth = document.getElementById('page').naturalWidth;
     }
 
+    panelRatio() {
+	return (this.imageWidth * this.panel.width) / (this.imageHeight * this.panel.height);
+    }
+
+    viewportRatio() {
+	return (this.viewportWidth / this.viewportHeight);
+    }
+
     focus() {
 	this.getViewportSize();
 	this.getImageSize();
-	
-	let zoom = 1;
-	let panel = { x: 50, y: 50, width: 100, height: 100 };
-	if (this.comic.currentPanel != 0) {
-	    console.log("Show panel");
-	    console.log("Drawing page:  "+this.comic.currentPage);
-	    console.log("Drawing panel: "+this.comic.currentPanel);
-	    panel = this.comic.pages[this.comic.currentPage].panels[this.comic.currentPanel];
+
+	console.log("Current panel: "+this.comic.currentPanel);
+
+	let scale = 1;
+	this.panel = { x: 50, y: 50, width: 100, height: 100 };
+	if (this.comic.currentPanel > -1) {
+	    this.panel = this.comic.pages[this.comic.currentPage].panels[this.comic.currentPanel];
 	} else {
 	    console.log("Show page");
 	}
-	zoom = this.imageWidth / panel.width * this.viewportWidth / this.imageWidth / 10;
-	console.log("imageWidht / panelWitth * viewportWidth / imageWidth / 10");
-	console.log(this.imageWidth+" / "+panel.width+" * "+this.viewportWidth+" / "+this.imageWidth+" / 10");
-	console.log(zoom);
-	console.log(panel);
-	// Not sure why I'm dividing by 10 here, and not 100, but it works???
+
+	console.log(this.panel);
+	console.log("Panel ratio "+this.panelRatio());
+	console.log("Viewport ratio "+this.viewportRatio());
+	if (this.panelRatio() <= this.viewportRatio()) {
+	    console.log("TALL");
+	    scale = 100 / this.panel.height * this.viewportHeight / this.imageHeight;
+	    console.log(100+" / "+this.panel.height+" * "+this.viewportHeight+" / "+ this.imageHeight);
+	} else {
+	    console.log("WIDE");
+	    scale = this.viewportWidth / this.imageWidth * 100 / this.panel.width;
+	    console.log(this.viewportWidth+" / "+this.imageWidth+" * "+100+" / "+this.panel.width);
+	}
+
+	console.log(scale);
 	anime({
 	    targets: "#page",
-	    scale: zoom,
-	    translateX: (50 - panel.x) + '%',
-	    translateY: (50 - panel.y) + '%',
-	    duration: 1000,
+	    scale: scale,
+	    translateX: (50 - this.panel.x) + '%',
+	    translateY: (50 - this.panel.y) + '%',
+	    duration: speed,
 	    easing: 'easeOutExpo',
 	    loop: false,
 	});
@@ -94,7 +114,7 @@ class Draw {
 class Comic {
     constructor(request) {
 	this.currentPage = 0;
-	this.currentPanel = 0;
+	this.currentPanel = -1;
 	console.log("COMIC");
 	request.addEventListener("progress", this.updateProgress);
 	request.addEventListener("load", this.transferComplete);
@@ -138,255 +158,6 @@ class Comic {
 	// Go to previous panel or page
     }
 }
-
-/*var w = 0;
-var h = 0;
-
-var pages = [];
-var page = 0;
-var panels = [];
-var pos = 0;
-
-var img;
-var prev;
-var menu;
-var next;
-
-var nexttime = false
-var menuvis = true
-*/
-
-// INIT
-
-/*window.onload = function() {
-    loadPages();
-    // readSVG();
-    assignButtons();
-}*/
-
-/*window.onresize = function() {
-    focus();
-}
-
-function init() {
-    oldspeed = speed;
-    speed = 1;
-    focus();
-    focus();
-    speed = oldspeed
-
-}
-
-function getSize() {
-    w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-    h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-    //console.log('Width: ' + w + ' - Height: '+ h);
-}
-
-function assignButtons() {
-    next = document.getElementById('nextbtn');
-    next.onclick = function () {
-	nextPanel();
-    }
-
-    menu = document.getElementById('menubtn');
-    menu.onclick = function () {
-	menuToggle();
-    }
-
-    prev = document.getElementById('prevbtn');
-    prev.onclick = function () {
-	prevPanel();
-    }
-}
-
-// MENU
-
-function menuHide() {
-    var hidemenu = anime({
-	targets: '#menu',
-	translateY: -60,
-	duration: speed,
-	easing: 'easeOutExpo',
-	loop: false
-    });
-}
-
-function menuShow() {
-    var showmenu = anime({
-	targets: '#menu',
-	translateY: 0,
-	duaration: speed,
-	easing: 'easeOutExpo',
-	loop: false
-    });
-}
-
-function menuToggle() {
-    if (menuvis == true) {
-	menuHide();
-    } else {
-	menuShow();
-    }
-    menuvis = !menuvis;
-}
-*/
-
-// Pages
-
-/*function loadPages() { 
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() {
-	if (this.readyState == 4 && this.status == 200) {
-	    storePages(JSON.parse(this.responseText));
-	}
-    };
-    xmlhttp.open("GET", "./pages.json", true);
-    xmlhttp.send();
-}
-
-function storePages(json) {
-    for (i = 0; i<json.pages.length; i++) {
-	p = {
-	    image: json.pages[i],
-	    svg: json.pages[i].replace(/\.[^\.]+$/, '.svg'),
-	    thumb: json.pages[i].replace(/\.[^\.]+$/, '-thumbnail.jpg'),
-	}
-	pages.push(p);
-    }
-    console.log(pages);
-    readSVG();
-}
-
-// SVG Parsing
-
-function sortPanels(a,b) {
-    if (a.id < b.id)
-	return -1;
-    if (a.id > b.id)
-	return 1;
-    return 0
-}
-
-
-function readSVG() {
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() {
-	if (this.readyState == 4 && this.status == 200) {
-	    parseSVG(this);
-	}
-    };
-    xmlhttp.open("GET", pages[page].svg, true);
-    xmlhttp.send();
-}
-
-
-function parseSVG(xml) {
-    var x, i, xmlDoc, txt;
-    panels = [];
-    img = document.getElementById('page');
-    xmlDoc = xml.responseXML;
-    svg = xmlDoc.getElementsByTagName('svg');
-    svgwidth = parseFloat(svg[0].getAttribute('width').replace(/[^0-9.]/g,''));
-    svgheight = parseFloat(svg[0].getAttribute('height').replace(/[^0-9.]/g,''));
-    rect = xmlDoc.getElementsByTagName("rect");
-
-    // Create the page as panel 0
-    var panel = {
-	id: '  the_page',
-	width: img.clientWidth,
-	height: img.clientHeight,
-	ratio: img.clientWidth / img.clientHeight,
-	x: '-50%',
-	y: '-50%'
-    }
-    panels.push(panel)
-
-    for (i = 0; i<rect.length; i++) {
-	panel = {};
-	panel.id = rect[i].getAttribute('id');
-	
-	// Get scale values
-	panel.height = (img.clientHeight / svgheight) * parseFloat(rect[i].getAttribute('height'));
-	panel.width = (img.clientWidth / svgwidth) * parseFloat(rect[i].getAttribute('width'));
-	panel.ratio = parseFloat(rect[i].getAttribute('width')) / parseFloat(rect[i].getAttribute('height'));
-
-	// Center of panel
-	x = parseFloat(rect[i].getAttribute('width')) / 2 + parseFloat(rect[i].getAttribute('x'));
-	// Center as percentage of page
-	x = (x / svgwidth * 100) * (-1);
-	panel.x = x + '%';
-
-	// Center of panel
-	y = parseFloat(rect[i].getAttribute('height')) / 2 + parseFloat(rect[i].getAttribute('y'));
-	// Center as percentage of page
-	y = y / svgheight * 100 * (-1);
-	panel.y = y + '%';
-
-	panels.push(panel)
-    }
-    panels.sort(sortPanels)
-    console.log(page)
-    console.log(panels);
-    init();
-}
-*/
-
-// NAVIGATION
-/*
-function focus() {
-    getSize();
-    var scale = 1.0;
-    var viewratio = (w / h);
-    //console.log(panels[pos])
-    if (panels[pos].ratio >= (w / h)) {
-	scale = w / panels[pos].width;
-    } else {
-	scale = h / panels[pos].height;
-    }
-    anime({
-	targets: ".page",
-	transformOrigin: panels[pos].x + ' ' + panels[pos].y,
-	scale: scale,
-	translateX: panels[pos].x,
-	translateY: panels[pos].y,
-	duration: speed,
-	easing: 'easeOutExpo',
-	loop: false,
-    });
-}
-
-
-function prevPanel() {
-    menuHide();
-    pos--;
-    if (pos < 0) {
-	// TODO! Go to previous page
-	page--;
-	readSVG();
-	pos = panels.length -1;
-	console.log("GOING TO PREVIOUS PAGE");
-	console.log(pos);
-	img.src = pages[page].image;
-    }
-    focus();
-}
-
-function nextPanel() {
-    menuHide();
-    pos++;
-    if (pos >= panels.length) {
-	// TODO! Go to next page
-	page++;
-	readSVG();
-	pos = 0;
-	console.log("GOING TO NEXT PAGE");
-	console.log(pos);
-	img.src = pages[page].image;
-    }
-    focus();
-}
-*/
 
 
 window.onload = function () {
