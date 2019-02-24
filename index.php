0b8a9ae3f0b668a1cc11a7718e051fe742acaaff
Ragnar Brynjúlfsson <ragtag@ragtag.net>
2019-02-24 23:01:50 +0100

    Removed nav tag and fixed indentation in index.php

--- index.php
+++ index.php
@@ -12,28 +12,26 @@
   </head>
   <body bgcolor="<?= $pbp->get_bgcolor() ?>">
     <div id="container">
-      <nav>
-	<a href="<?= $pbp->get_prev() ?>" class="navbtn" id="prevbtn"></a>
-	<a href="#" class="navbtn" id="menubtn"></a>
-	<a href="<?= $pbp->get_next() ?>" class="navbtn" id="nextbtn"></a>
-	<div id="menu">
-	  <div id="menu-left">
-	    <a href="<?= $pbp->get_home() ?>" id="homebtn">
-	      <img src="images/back.svg" />
-	    </a>
-	  </div>
-	  <div id="menu-center">
-	    <a href="#" id="pbpbtn">
-	      <img src="images/panel-by-panel.svg" alt="Toggle Panel by Panel navigation" />
-	    </a>
-	  </div>
-	  <div id="menu-right">
-	    <a href="<?= $pbp->get_thumbs() ?>" id="thumbsbtn"">
-	      <img src="images/pages.svg" />
-	    </a>
-	  </div>
+      <a href="<?= $pbp->get_prev() ?>" class="navbtn" id="prevbtn"></a>
+      <a href="#" class="navbtn" id="menubtn"></a>
+      <a href="<?= $pbp->get_next() ?>" class="navbtn" id="nextbtn"></a>
+      <div id="menu">
+	<div id="menu-left">
+	  <a href="<?= $pbp->get_home() ?>" id="homebtn">
+	    <img src="images/back.svg" />
+	  </a>
 	</div>
-      </nav>
+	<div id="menu-center">
+	  <a href="#" id="pbpbtn">
+	    <img src="images/panel-by-panel.svg" alt="Toggle Panel by Panel navigation" />
+	  </a>
+	</div>
+	<div id="menu-right">
+	  <a href="<?= $pbp->get_thumbs() ?>" id="thumbsbtn" alt="Show page overview">
+	    <img src="images/pages.svg" />
+	  </a>
+	</div>
+      </div>
     </div>
     <div id="pagecontainer">
       <img src="<?= $pbp->get_image() ?>"
