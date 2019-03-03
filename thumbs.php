592c7da7e89aa5cacf97a36580d6be47e6bd33b8
Ragnar Brynjúlfsson <ragtag@ragtag.net>
2019-03-03 17:22:07 +0100

    Implemented pretty URLs using .htaccess for the PHP version.

--- thumbs.php
+++ thumbs.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="initial-scale=1, maximum-scale=1">
     <?php require "panel-by-panel.php" ?>
     <title><?= $pbp->get_title($page, $acbf) ?></title>
-    <link rel="stylesheet" href="style.css">
-    <noscript><link rel="stylesheet" href="noscript.css"></noscript>
+    <link rel="stylesheet" href="/style.css">
+    <noscript><link rel="stylesheet" href="/noscript.css"></noscript>
   </head>
   <body bgcolor="<?= $pbp->get_thumbs_bgcolor($page, $acbf) ?>">
   <div id="thumbs">
